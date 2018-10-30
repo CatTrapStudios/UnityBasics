766cb6fef53e9a79cddc8eae7d7a7b97c33e0e1f
David Ward <dwardprg@gmail.com>
2018-10-30 19:58:36 +0000

    Change directory structure to use many projects. Its cleaner than using one

---
+++ UnityBasics/3. Animation and Animators/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private const float SPEED = 2.5f;
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= SPEED * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += SPEED * Time.deltaTime;
+        }
+
+        transform.position = position;
+    }
+}

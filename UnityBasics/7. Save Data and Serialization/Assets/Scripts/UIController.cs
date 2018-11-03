b90e1ab0d729cb4cb044878ac7fc125fba546f68
David Ward <dwardprg@gmail.com>
2018-11-03 08:53:08 +0000

    Adding 2 tutorials and background image to all tutorials

---
+++ UnityBasics/7. Save Data and Serialization/Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+
+    public Text score;
+    private int remainingLives = 100;
+
+    private void Start()
+    {
+        UpdateScore();
+    }
+
+    public void PauseButtonClicked()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        resumeButton.SetActive(true);
+    }
+
+    public void PlayButtonClicked()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        resumeButton.SetActive(false);
+    }
+
+    public void ArrowHitPlayer()
+    {
+        remainingLives--;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        score.text = remainingLives.ToString();
+    }
+}

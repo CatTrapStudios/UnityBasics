13247de68e09be628be3571fa1a969fd8b685c11
David Ward <dwardprg@gmail.com>
2018-11-03 09:48:24 +0000

    Adding serialization tutorial

---
+++ UnityBasics/8. Audio/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private const float SPEED = 2.5f;
+
+    public UIController UI;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= SPEED * Time.deltaTime;
+            animator.SetBool("walking", true);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += SPEED * Time.deltaTime;
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+
+        transform.position = position;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") ||
+               animator.GetCurrentAnimatorStateInfo(0).IsName("Move") ||
+               animator.GetNextAnimatorStateInfo(0).IsName("Idle") ||
+               animator.GetCurrentAnimatorStateInfo(0).IsName("Move"))
+            {
+                animator.SetTrigger("jump");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        UI.ArrowHitPlayer();
+    }
+}
---
+++ UnityBasics/8. Audio/Assets/Scripts/SaveDataSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+
+public class SaveDataSerializer
+{
+    private static string SAVE_PATH = Application.persistentDataPath + "/SaveData.xml";
+
+    public void Save(SaveData data)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(SaveData));
+        FileStream stream = new FileStream(SAVE_PATH, FileMode.Create);
+        serializer.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public SaveData Load()
+    {
+        if (File.Exists(SAVE_PATH) == false)
+        {
+            return null;
+        }
+
+        XmlSerializer serializer = new XmlSerializer(typeof(SaveData));
+        FileStream stream = new FileStream(SAVE_PATH, FileMode.Open);
+        SaveData saveData = serializer.Deserialize(stream) as SaveData;
+        stream.Close();
+
+        return saveData;
+    }
+}
